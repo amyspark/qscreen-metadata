fd121e7670d0b6b9af5d4a1d71932c295dee76d0
L. E. Segovia <amy@amyspark.me>
2023-03-09 17:37:11 +0000

    Complete macOS EDID parsing support

--- macos.mm
+++ macos.mm
@@ -1,55 +1,111 @@
 #include "screens.h"
+
 #include <AppKit/Appkit.h>
 #include <CoreGraphics/CoreGraphics.h>
+#include <IOKit/graphics/IOGraphicsLib.h>
 
+#include <QApplication>
 #include <QWidget>
+#include <QWindow>
+#include <private/qedidparser_p.h>
 
-static QString displayName(CGDirectDisplayID displayID)
+using kvmap = std::map<QString, QString>;
+
+static kvmap edidForDisplay(CGDirectDisplayID displayID)
 {
-    QIOType<io_iterator_t> iterator;
-    if (IOServiceGetMatchingServices(kIOMasterPortDefault,
-        IOServiceMatching("IODisplayConnect"), &iterator))
-        return QString();
+    const auto vendor = CGDisplayVendorNumber(displayID);
 
-    QIOType<io_service_t> display;
+    const auto model = CGDisplayModelNumber(displayID);
+
+    const auto serialNumber = CGDisplaySerialNumber(displayID);
+
+    io_iterator_t iterator{};
+    if (IOServiceGetMatchingServices(kIOMasterPortDefault,
+        IOServiceMatching("IODisplayConnect"), &iterator) != KERN_SUCCESS)
+        return {};
+
+    io_service_t display{};
     while ((display = IOIteratorNext(iterator)) != 0)
     {
-        NSDictionary *info = [(__bridge NSDictionary*)IODisplayCreateInfoDictionary(
+        const NSDictionary *info = [(__bridge NSDictionary*)IODisplayCreateInfoDictionary(
             display, kIODisplayOnlyPreferredName) autorelease];
 
-        if ([[info objectForKey:@kDisplayVendorID] longValue] != CGDisplayVendorNumber(displayID))
+        if ([[info objectForKey:@kDisplayVendorID] unsignedIntValue] != vendor
+            || [[info objectForKey:@kDisplayProductID] unsignedIntValue] != model
+            || [[info objectForKey:@kDisplaySerialNumber] unsignedIntValue] != serialNumber)
             continue;
 
-        if ([[info objectForKey:@kDisplayProductID] longValue] != CGDisplayModelNumber(displayID))
-            continue;
+        const QByteArray edidData = QByteArray::fromCFData((__bridge CFDataRef)[info objectForKey:@kIODisplayEDIDKey]);
 
-        if ([[info objectForKey:@kDisplaySerialNumber] longValue] != CGDisplaySerialNumber(displayID))
-            continue;
+        QEdidParser edid;
 
-        NSDictionary *localizedNames = [info objectForKey:@kDisplayProductName];
-        if (![localizedNames count])
-            break; // Correct screen, but no name in dictionary
+        if (!edid.parse(edidData))
+            break;
 
-        return QString::fromNSString([localizedNames objectForKey:[[localizedNames allKeys] objectAtIndex:0]]);
+        return {
+            {QString("name"), edid.identifier},
+            {QString("manufacturer"), edid.manufacturer},
+            {QString("model"), edid.model},
+            {QString("serialNumber"), edid.serialNumber}
+        };
     }
 
-    return QString();
+    return {};
+}
+
+static NSNumber *screenId()
+{
+    const NSScreen *screen = [&]() {
+        const QWidget *wdg = QApplication::activeWindow();
+
+        if (!wdg)
+            return [NSScreen mainScreen];
+
+        const NSView *view = (NSView*)wdg->windowHandle()->winId();
+
+        if (!view)
+            return [NSScreen mainScreen];
+
+        const NSWindow *window = [view window];
+
+        if (!window)
+            return [NSScreen mainScreen];
+
+        return [window screen];
+    }();
+
+    return (NSNumber *)[[screen deviceDescription] valueForKey:@"NSScreenNumber"];
 }
 
 QString CrossPlatformScreen::name() const
 {
-    // Using the NSScreen localizedName here would return a localized name
-    // Unsure why Qt returns the correct device name, but alas
+    const NSNumber *id = screenId();
+
+    if (id != nullptr) {
+        const auto data = edidForDisplay([id unsignedIntValue]);
+
+        const auto x = data.find("name");
+
+        if (x != data.end() && !x->second.isEmpty()) {
+            return x->second;
+        }
+    }
+
     return this->screen->name();
 }
 
 QString CrossPlatformScreen::manufacturer() const
 {
-    QPlatformScreen *nativeScreen = screen->handle();
+    const NSNumber *id = screenId();
 
-    NSNumber *id = (NSNumber *)[[screen deviceDescription] valueForKey:@"NSScreenNumber"];
     if (id != nullptr) {
-        return QString::number(CGDisplayVendorNumber([id unsignedIntValue]), 16);
+        const auto data = edidForDisplay([id unsignedIntValue]);
+
+        const auto x = data.find("manufacturer");
+
+        if (x != data.end() && !x->second.isEmpty()) {
+            return x->second;
+        }
     }
 
     return this->screen->manufacturer();
@@ -57,11 +113,16 @@ QString CrossPlatformScreen::manufacturer() const
 
 QString CrossPlatformScreen::model() const
 {
-    NSScreen *screen = [NSScreen mainScreen];
-    NSNumber *id = (NSNumber *)[[screen deviceDescription] valueForKey:@"NSScreenNumber"];
+   const NSNumber *id = screenId();
 
     if (id != nullptr) {
-        return QString::number(CGDisplayModelNumber([id unsignedIntValue]), 16);
+        const auto data = edidForDisplay([id unsignedIntValue]);
+
+        const auto x = data.find("model");
+
+        if (x != data.end() && !x->second.isEmpty()) {
+            return x->second;
+        }
     }
 
     return this->screen->model();
@@ -69,11 +130,16 @@ QString CrossPlatformScreen::model() const
 
 QString CrossPlatformScreen::serialNumber() const
 {
-    NSScreen *screen = [NSScreen mainScreen];
-    NSNumber *id = (NSNumber *)[[screen deviceDescription] valueForKey:@"NSScreenNumber"];
+    const NSNumber *id = screenId();
 
     if (id != nullptr) {
-        return QString::number(CGDisplaySerialNumber([id unsignedIntValue]), 16);
+        const auto data = edidForDisplay([id unsignedIntValue]);
+
+        const auto x = data.find("serialNumber");
+
+        if (x != data.end() && !x->second.isEmpty()) {
+            return x->second;
+        }
     }
 
     return this->screen->serialNumber();
